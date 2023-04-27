ae5896c8699fc19314dfc59ab6c1c8e18e66e22f
Natedawg2077 <NatedawgGDEV@gmail.com>
2023-04-27 08:40:13 -0400

    Restart button

---
+++ Assets/scripts/UI/RestartButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour
+{
+    public void Restart()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if(sceneName != "Paused")
+        {
+            SceneManager.UnloadSceneAsync(currentScene);
+            SceneManager.LoadScene(sceneName);
+            SceneManager.LoadScene("InLevel", LoadSceneMode.Additive);
+            SceneManager.UnloadSceneAsync("Paused");
+        }
+
+    }
+}
