11b786bbf37332e0d87159341a36c845f579f927
Noxx <78322656+ImEcto@users.noreply.github.com>
2023-04-27 08:55:32 -0400

    timer

--- Assets/scripts/other/Timer.cs
+++ Assets/scripts/other/Timer.cs
@@ -4,7 +4,9 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public Text timerText;
+    public Text gameOverText;
     private float startTime;
+    private float totalTime = 10.0f;
 
     void Start()
     {
@@ -13,9 +15,18 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-        float timeElapsed = Time.time - startTime;
-        string minutes = ((int)timeElapsed / 60).ToString();
-        string seconds = (timeElapsed % 60).ToString("f2");
-        timerText.text = minutes + ":" + seconds;
+        float timeLeft = totalTime - (Time.time - startTime);
+        if (timeLeft < 0.0f)
+        {
+            timeLeft = 0.0f;
+            Time.timeScale = 0.0f; // Freeze the game
+            gameOverText.text = "Game Over"; // Show the "Game Over" message
+        }
+        else
+        {
+            string minutes = ((int)timeLeft / 60).ToString();
+            string seconds = (timeLeft % 60).ToString("f2");
+            timerText.text = minutes + ":" + seconds;
+        }
     }
 }
