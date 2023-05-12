764ea0d47cb0b933f65364e282a02923409fd271
Noxx <78322656+ImEcto@users.noreply.github.com>
2023-05-12 08:35:33 -0400

    timer n win

--- Assets/scripts/other/win.cs
+++ Assets/scripts/other/win.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class win : MonoBehaviour
 {
+<<<<<<< Updated upstream
+=======
+    public Text text; // Reference to the text UI component in the Inspector
+    
+    
+>>>>>>> Stashed changes
 
     void Start()
     {
